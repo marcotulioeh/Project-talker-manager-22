941a0069b9c2d50ce61c3a8a7ccd7c339f44e517
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-08-02 21:23:35 -0300

    feat: adiciona endpoint GET /talker.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
+const { getFsTalker } = require('./fsTalker');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,10 +9,15 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar!
+// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', rescue(async (_req, res) => {
+  const talker = await getFsTalker();
+  res.status(200).json(talker);
+}));
 
 app.listen(PORT, () => {
   console.log('Online');
