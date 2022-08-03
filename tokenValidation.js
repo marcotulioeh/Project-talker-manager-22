fa7760af29fbe8f9fd59855bfb81a1b64e6a3445
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-08-03 04:26:22 -0300

    feat: adiciona funções de validações de nome, idade, talk, watchedAt e rate.

--- tokenValidation.js
+++ tokenValidation.js
@@ -20,19 +20,80 @@ const checkPassword = (req, res, next) => {
   next();
 };
 
-// const checkToken = (req, res, next) => {
-//   const token = req.headers.authorization;
-//   if (!token || token === '') {
-//     return res.status(401).json({ message: 'Token não existe.' });
-//   }
-//   if (token.length !== 16) {
-//     return res.status(401).json({ message: 'Este Token não é válido.' });
-//   }
-//   next();
-// };
+const checkToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token || token === '') {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+  if (token.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const checkName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name === '') {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const checkAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const checkTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || talk === '') {
+    return res.status(400)
+    .json({ message: 'O campo "talk" é obrigatório' });
+  }
+  next();
+};
+
+const checkRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (!rate && rate !== 0) {
+    return res.status(400)
+    .json({ message: 'O campo "rate" é obrigatório' });
+  }
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const checkWatchedAt = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  if (!watchedAt || watchedAt === '') {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+  const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+  if (!regex.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
 
 module.exports = {
   checkEmail,
   checkPassword,
-  // checkToken,
+  checkToken,
+  checkName,
+  checkAge,
+  checkTalk,
+  checkRate,
+  checkWatchedAt,
 };
